db64852f02bf170872ca9302fa37531b5a7ce4a2
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-03-30 16:36:09 +0530

    Search

--- src/components/DpcList.jsx
+++ src/components/DpcList.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { DpcItem } from "./DpcItem";
+import { TextField, Card, CardContent } from "@mui/material";
 
 export const DpcList = ({ driverdata }) => {
-  const [data, setData] = useState([...driverdata]);
+  const [data, setData] = useState([]);
+  const [txtSrc, setTxtSrc] = useState("");
+
   useEffect(() => {
-    setData([...driverdata]);
-  }, [driverdata]);
+    const filtered = driverdata.filter((item) =>
+      item.fname.toUpperCase().includes(txtSrc.toUpperCase())
+    );
+    setData([...filtered]);
+  }, [txtSrc]);
+
   return (
     <React.Fragment>
+      <Card>
+        <CardContent>
+          <TextField
+            label="Search DPC"
+            onChange={(e) => setTxtSrc(e.target.value)}
+          />
+        </CardContent>
+      </Card>
       {data.map((item, index) => (
         <DpcItem key={index} item={item} />
       ))}
