3e513dc075fdeeccb736a9d40dd774c3ca038f37
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-04-03 16:14:09 +0530

    Eshp

--- src/components/EShop.jsx
+++ src/components/EShop.jsx
@@ -5,13 +5,18 @@ import {
   Card,
   CardContent,
   Grid,
-  CardMedia,
+  TextField,
   Badge,
 } from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 
 export const EShop = () => {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [filtProducts, setFiltProducts] = useState([]);
+  const [cartdata, setCartdata] = useState([]);
+  const [selectedCat, setSelectedCat] = useState("All");
   const getCategories = async () => {
     const catdata = await axios.get(
       "https://fakestoreapi.com/products/categories"
@@ -21,7 +26,21 @@ export const EShop = () => {
   const getProducts = async () => {
     const proddata = await axios.get("https://fakestoreapi.com/products");
     setProducts([...proddata.data]);
+    setFiltProducts([...proddata.data]);
   };
+  const handleAddToCart = (item) => {
+    setCartdata([item, ...cartdata]);
+  };
+  useEffect(() => {
+    if (selectedCat === "All") {
+      setFiltProducts([...products]);
+    } else {
+      const filtered = products.filter(
+        (item) => item.category.toUpperCase() === selectedCat.toUpperCase()
+      );
+      setFiltProducts([...filtered]);
+    }
+  }, [selectedCat]);
   useEffect(() => {
     if (categories.length > 0) {
       getProducts();
@@ -33,17 +52,25 @@ export const EShop = () => {
   return (
     <React.Fragment>
       <Card>
-        <CardContent>
+        <CardContent className="submenu">
           {categories.map((item) => (
-            <Button variant="contained" className="catitem">
+            <Button
+              variant="contained"
+              className="catitem"
+              onClick={() => setSelectedCat(item)}
+            >
               {item}
             </Button>
           ))}
+          <TextField label="Search" variant="outlined" className="searchbox" />
+          <Badge color="success" badgeContent={cartdata.length}>
+            <ShoppingCartOutlinedIcon className="carting" />
+          </Badge>
         </CardContent>
       </Card>
       <br />
       <Grid container spacing={3}>
-        {products.map((item) => (
+        {filtProducts.map((item) => (
           <Grid item xs={3}>
             <Card className="containeritem">
               <CardContent>
@@ -51,6 +78,10 @@ export const EShop = () => {
                   {item.title.substring(0, 25)}
                   {item.title.length > 30 && "..."}
                 </h3>
+                <AddShoppingCartOutlinedIcon
+                  className="cartingadd"
+                  onClick={() => handleAddToCart(item)}
+                />
                 <Badge color="secondary" badgeContent={"$" + item.price}>
                   <img src={item.image} className="imgitem" />
                 </Badge>
